bfbef28c10b1f6e9aafdf0dac0016564f52c8c52
Dmitry <df.creative@gmail.com>
2017-05-29 18:57:30 -0400

    Add interactions

--- demo.js
+++ demo.js
@@ -4,6 +4,7 @@ const createCluster = require('./')
 const getContext = require('get-canvas-context')
 const panZoom = require('pan-zoom')
 const createLoop = require('canvas-loop')
+const fps = require('fps-indicator')()
 
 
 //data points
@@ -15,7 +16,10 @@ let points = Array(N*2).fill(null).map(_ => [Math.random(), Math.random()])
 let cluster = createCluster(points)
 
 
+
 //rendering loop
+let scale = 1, offset = [0, 0]
+
 let ctx = getContext('2d')
 let canvas = ctx.canvas
 document.body.appendChild(canvas)
@@ -25,17 +29,37 @@ let app = createLoop(canvas)
 app.start()
 
 app.on('tick', dt => {
+	render()
 })
 
 app.on('resize', _ => {
-	render()
 })
-render()
+
+
+//interactions
+panZoom(canvas, e => {
+	let w = canvas.width
+	let h = canvas.height
+
+	offset[0] += e.dx
+	offset[1] += e.dy
+
+	let rx = e.x / w
+	let ry = e.y / w
+
+	let prevScale = scale
+	scale -= scale * e.dz / 1e3
+
+	offset[0] -= rx * w * (scale - prevScale)
+	offset[1] -= ry * w * (scale - prevScale)
+})
 
 
 function render () {
 	let w = canvas.width
 	let h = canvas.height
+
+	ctx.clearRect(0,0,w,h)
 
 	ctx.fillStyle = 'rgba(0,100,200,.75)';
 	let diameter = 10
@@ -45,10 +69,12 @@ function render () {
 		let y = points[i][1]
 
 		ctx.beginPath()
-		ctx.arc(x * w, y * h, diameter/2, 0, 2 * Math.PI)
+		ctx.arc(x * w * scale + offset[0], y * h * scale + offset[1], diameter/2, 0, 2 * Math.PI)
 		ctx.closePath()
 
 		ctx.fill();
 	}
 
 }
+
+
